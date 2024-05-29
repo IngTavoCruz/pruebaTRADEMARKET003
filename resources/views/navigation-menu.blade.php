346ff35d54526513a903e3458484e617b005d344
unknown <mundo_cube@hotmail.com>
2024-05-29 14:13:53 -0600

    prueba tecnica desarrollador back end

--- resources/views/navigation-menu.blade.php
+++ resources/views/navigation-menu.blade.php
@@ -3,12 +3,12 @@
     <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div class="flex justify-between h-16">
             <div class="flex">
-                
+                        
                 <!-- Navigation Links -->
                 <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
                     <x-nav-link href="{{ route('tareas.index') }}" :active="request()->routeIs('tareas.index')">
                         {{ __('Lista de Tareas') }}
-                    </x-nav-link>
+                    </x-nav-link>      
                 </div>
             </div>
 
